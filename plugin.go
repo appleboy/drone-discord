12ff54fee00723a20ec1c0a844149efe813f13ae
Bo-Yi Wu <appleboy.tw@gmail.com>
2017-06-14 11:14:32 +0800

    add comment
    Signed-off-by: Bo-Yi Wu <appleboy.tw@gmail.com>

--- plugin.go
+++ plugin.go
@@ -41,30 +41,36 @@ type (
 		Message      []string
 	}
 
+	// EmbedFooterObject for Embed Footer Structure.
 	EmbedFooterObject struct {
 		Text string `json:"text"`
 	}
 
+	// EmbedAuthorObject for Embed Author Structure
 	EmbedAuthorObject struct {
 		Name    string `json:"name"`
 		URL     string `json:"url"`
 		IconURL string `json:"icon_url"`
 	}
 
+	// EmbedFieldObject for Embed Field Structure
 	EmbedFieldObject struct {
 		Name  string `json:"name"`
 		Value string `json:"value"`
 	}
 
+	// EmbedObject is for Embed Structure
 	EmbedObject struct {
 		Title       string              `json:"title"`
 		Description string              `json:"description"`
 		URL         string              `json:"url"`
+		Color       int                 `json:"color"`
 		Footer      *EmbedFooterObject  `json:"footer"`
 		Author      *EmbedAuthorObject  `json:"author"`
 		Fields      []*EmbedFieldObject `json:"fields"`
 	}
 
+	// Payload struct
 	Payload struct {
 		Wait      bool                  `json:"wait"`
 		Content   string                `json:"content"`
