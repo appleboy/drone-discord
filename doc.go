4d1d86d8c26dea05daf58cf491cd1f68dd415de3
appleboy <appleboy.tw@gmail.com>
2025-03-08 16:35:19 +0800

    docs: clean up documentation by removing unnecessary comments
    - Remove a commented-out line from the documentation file
    
    Signed-off-by: appleboy <appleboy.tw@gmail.com>

--- doc.go
+++ doc.go
@@ -2,19 +2,18 @@
 //
 // Details about the drone-discord project are found in github page:
 //
-//     https://github.com/appleboy/drone-discord
+//	https://github.com/appleboy/drone-discord
 //
 // The pre-compiled binaries can be downloaded from release page.
 //
 // Send Notification
 //
-//   drone-discord \
-//     --webhook-id xxxx \
-//     --webhook-token xxxx \
-//     --username value \
-//     --avatar-url value \
-//     --message "Test Message"
+//	drone-discord \
+//	  --webhook-id xxxx \
+//	  --webhook-token xxxx \
+//	  --username value \
+//	  --avatar-url value \
+//	  --message "Test Message"
 //
 // For more details, see the documentation and example.
-//
 package main
